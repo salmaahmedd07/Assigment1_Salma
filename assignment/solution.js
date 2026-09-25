2f718a626714b786eb9a367c996a01ec5f951e01
Salma Ahmed Hassan <salma.ahmedhassan@student.giu-uni.de>
2026-09-25 21:25:26 +0300

    add the solution

---
+++ assignment/solution.js
@@ -0,0 +1,7 @@
+const heroName = "salma ahmed hassan the Mage";
+const studentId = 16001631;
+const power = studentId * 9;
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log("By the Sinai dunes, the Sand Kraken kneels!");
